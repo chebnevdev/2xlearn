6f72935b8925ae16cfcd1e56cb51c253359b09d3
ChebnevDev <chebnevdev@gmail.com>
2019-12-09 00:04:17 +0300

    Added field for full information, footer and changed styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,57 @@
-function getInfo(tech) {
+var modal = document.getElementById("myModal");
+var techHeader = document.getElementById("technique-header");
+var techBody = document.getElementById("technique-body");
+var techFooter = document.getElementById("technique-footer");
 
-  toggleDescription(true);
+function getInfo(technique) {
+	let headerContent;
+	let bodyContent;
+	let footerContent;
+	
+	switch (technique) {
+		case "pomodoro":
+			headerContent = "The Pomodoro Technique";
+			bodyContent = `
+				<p>This technique helps you focus on the problem by these simple steps:</p>
+				<ul>
+					<li>Disable notifications or any other sources of distraction</li>
+					<li>Set up a timer for 25 minutes</li>
+					<li>Start working on the problem</li>
+				</ul>
+				<p>After completing the pomodoro take a break for 5 minutes, reward yourself somehow 
+				(for example, check mail or surf the web a bit) and then go to the next pomodoro.</p>
+`;
+			footerContent = "Rewarding yourself is very important for this technique to strengthen!";
+			break;
+		case "mempfal":
+			headerContent = "The Memory Palace";
+			bodyContent = "";
+			footerContent = "Don't forget to reward yourself afterwards!";
+			break;
+		default:
+			headerContent = "What's that?";
+			bodyContent = "<h4>Whoa?</h4>";
+			footerContent = "I donno know...";
+			break;
+	}
+	techHeader.textContent = headerContent;
+	techBody.innerHTML = bodyContent;
+	techFooter.textContent = footerContent;
+	// When the user clicks the button, open the modal 
+	modal.style.display = "block";
 }
 
-function toggleDescription(show) {
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  let descDiv = document.getElementById('desc-div');
-  let compStyle = window.getComputedStyle(descDiv, null);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-  let height = compStyle.getPropertyValue('height');
-  height = parseInt(height.slice({ to: height.length-3}));
-
-  let paddingTop = compStyle.getPropertyValue('padding-top');
-  paddingTop = parseInt(paddingTop.slice({ to: paddingTop.length-3}));
-
-  let paddingBottom = compStyle.getPropertyValue('padding-bottom');
-  paddingBottom = parseInt(paddingBottom.slice({ to: paddingBottom.length-3}));
-
-  //alert(`Full size: ${height + paddingBottom + paddingTop} px`);
-
-
-  if (show) {
-
-  } else {
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
